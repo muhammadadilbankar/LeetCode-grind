78dacd624dd10bb2c3af09d4cee208121943d6d4
muhammadadilbankar <140646669+muhammadadilbankar@users.noreply.github.com>
2025-05-09 23:12:24 +0530

    Time: 189 ms (100%), Space: 48.8 MB (0%) - LeetHub

---
+++ 3637-count-number-of-balanced-permutations/3637-count-number-of-balanced-permutations.cs
@@ -0,0 +1,80 @@
+using System;
+
+public class Solution {
+    private const long MOD = 1_000_000_007;
+    private long[,,] memo;
+    private int[] cnt;
+    private int[] psum;
+    private int target;
+    private long[,] comb;
+
+    public int CountBalancedPermutations(string num) {
+        int tot = 0, n = num.Length;
+        cnt = new int[10];
+        foreach (char ch in num) {
+            int d = ch - '0';
+            cnt[d]++;
+            tot += d;
+        }
+        if (tot % 2 != 0) {
+            return 0;
+        }
+
+        target = tot / 2;
+        int maxOdd = (n + 1) / 2;
+
+        /* Pre-calculate combinations */
+        comb = new long[maxOdd + 1, maxOdd + 1];
+        for (int i = 0; i <= maxOdd; i++) {
+            comb[i, i] = comb[i, 0] = 1;
+            for (int j = 1; j < i; j++) {
+                comb[i, j] = (comb[i - 1, j] + comb[i - 1, j - 1]) % MOD;
+            }
+        }
+
+        psum = new int[11];
+        for (int i = 9; i >= 0; i--) {
+            psum[i] = psum[i + 1] + cnt[i];
+        }
+
+        memo = new long[10, target + 1, maxOdd + 1];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= target; j++) {
+                for (int k = 0; k <= maxOdd; k++) {
+                    memo[i, j, k] = -1;
+                }
+            }
+        }
+
+        return (int)Dfs(0, 0, maxOdd);
+    }
+
+    private long Dfs(int pos, int curr, int oddCnt) {
+        /* If the remaining positions cannot be legally filled, or if the sum of
+         * the elements at the current odd positions is greater than the target
+         * value */
+        if (oddCnt < 0 || psum[pos] < oddCnt || curr > target) {
+            return 0;
+        }
+        if (pos > 9) {
+            return (curr == target && oddCnt == 0) ? 1 : 0;
+        }
+        if (memo[pos, curr, oddCnt] != -1) {
+            return memo[pos, curr, oddCnt];
+        }
+        /* Even-numbered positions remaining to be filled */
+        int evenCnt = psum[pos] - oddCnt;
+        long res = 0;
+        for (int i = Math.Max(0, cnt[pos] - evenCnt);
+             i <= Math.Min(cnt[pos], oddCnt); i++) {
+            /* The current digit is filled with i positions at odd positions,
+             * and cnt[pos] - i positions at even positions */
+            long ways = comb[oddCnt, i] * comb[evenCnt, cnt[pos] - i] % MOD;
+            res =
+                (res + ways * Dfs(pos + 1, curr + i * pos, oddCnt - i) % MOD) %
+                MOD;
+        }
+        memo[pos, curr, oddCnt] = res;
+        return res;
+    }
+}
