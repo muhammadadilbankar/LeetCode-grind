41094a07510dcc435c2d407fa1cb03daa46c61a0
muhammadadilbankar <140646669+muhammadadilbankar@users.noreply.github.com>
2025-06-29 17:36:55 +0530

    Time: 0 ms (100%), Space: 18.7 MB (24.39%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<pair<int, int>> pos;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == 0)
-                pos.push_back({i, j});
+        //O(1) solution
+        //int col[m] = {0}; -> matrix[0][..]
+        //int row[n] = {0}; -> matrix[..][0]
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int col0 = 1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    //mark the ith row
+                    matrix[i][0] = 0;
+                    if(j != 0) matrix[0][j] = 0;
+                    else col0 = 0;
+                }
             }
         }
-        for(auto p : pos){
-            int y = p.first;
-            int x = p.second;
-            for (int col = 0; col < n; col++) matrix[y][col] = 0; // zero row y
-            for (int row = 0; row < m; row++) matrix[row][x] = 0; // zero column x
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int j=0; j<m; j++) matrix[0][j] = 0;
+        }
+        if(col0 == 0){
+            for(int i=0; i<n; i++){
+                matrix[i][0] = 0;
+            }
         }
         return;
     }
